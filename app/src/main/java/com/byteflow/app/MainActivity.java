02b8d8f36e98f98a296aaada9cba332dde0d4204
lyf <1374329135@qq.com>
2021-01-02 10:20:40 +0800

    首次提交

---
+++ app/src/main/java/com/byteflow/app/MainActivity.java
@@ -0,0 +1,217 @@
+package com.byteflow.app;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.byteflow.app.adapter.MyRecyclerViewAdapter;
+import com.byteflow.app.audio.AudioCollector;
+import com.byteflow.app.egl.EGLActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+import static android.opengl.GLSurfaceView.RENDERMODE_CONTINUOUSLY;
+import static android.opengl.GLSurfaceView.RENDERMODE_WHEN_DIRTY;
+import static com.byteflow.app.MyNativeRender.SAMPLE_TYPE;
+
+import static com.byteflow.app.MyNativeRender.SAMPLE_TYPE_TRIANGLE;
+
+public class MainActivity extends AppCompatActivity implements AudioCollector.Callback, ViewTreeObserver.OnGlobalLayoutListener {
+    private static final String TAG = "MainActivity";
+    private static final String[] REQUEST_PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO,
+    };
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private static final String[] SAMPLE_TITLES = {
+            "DrawTriangle"
+    };
+
+    private MyGLSurfaceView mGLSurfaceView;
+    private ViewGroup mRootView;
+    private int mSampleSelectedIndex = SAMPLE_TYPE;
+    private MyGLRender mGLRender = new MyGLRender();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRootView = findViewById(R.id.rootView);
+        mRootView.getViewTreeObserver().addOnGlobalLayoutListener(this);
+        mGLRender.init();
+        mGLRender.setParamsInt(SAMPLE_TYPE, SAMPLE_TYPE, 0);
+
+    }
+
+    @Override
+    public void onGlobalLayout() {
+        mRootView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+        mGLSurfaceView = new MyGLSurfaceView(this, mGLRender);
+        mRootView.addView(mGLSurfaceView, lp);
+        mGLSurfaceView.setRenderMode(RENDERMODE_CONTINUOUSLY);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!hasPermissionsGranted(REQUEST_PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, REQUEST_PERMISSIONS, PERMISSION_REQUEST_CODE);
+        }
+        CommonUtils.copyAssetsDirToSDCard(MainActivity.this, "poly", "/sdcard/model");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (!hasPermissionsGranted(REQUEST_PERMISSIONS)) {
+                Toast.makeText(this, "We need the permission: WRITE_EXTERNAL_STORAGE", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mGLRender.unInit();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_change_sample) {
+            showGLSampleDialog();
+        }
+        return true;
+    }
+
+    @Override
+    public void onAudioBufferCallback(short[] buffer) {
+        Log.e(TAG, "onAudioBufferCallback() called with: buffer[0] = [" + buffer[0] + "]");
+        mGLRender.setAudioData(buffer);
+        //mGLSurfaceView.requestRender();
+    }
+
+
+    private void showGLSampleDialog() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        final View rootView = inflater.inflate(R.layout.sample_selected_layout, null);
+
+        final AlertDialog dialog = builder.create();
+
+        Button confirmBtn = rootView.findViewById(R.id.confirm_btn);
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+
+        final RecyclerView resolutionsListView = rootView.findViewById(R.id.resolution_list_view);
+
+        final MyRecyclerViewAdapter myPreviewSizeViewAdapter = new MyRecyclerViewAdapter(this, Arrays.asList(SAMPLE_TITLES));
+        myPreviewSizeViewAdapter.setSelectIndex(mSampleSelectedIndex);
+        myPreviewSizeViewAdapter.addOnItemClickListener(new MyRecyclerViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                mRootView.removeView(mGLSurfaceView);
+                RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+                mGLSurfaceView = new MyGLSurfaceView(MainActivity.this, mGLRender);
+                mRootView.addView(mGLSurfaceView, lp);
+
+                int selectIndex = myPreviewSizeViewAdapter.getSelectIndex();
+                myPreviewSizeViewAdapter.setSelectIndex(position);
+                myPreviewSizeViewAdapter.notifyItemChanged(selectIndex);
+                myPreviewSizeViewAdapter.notifyItemChanged(position);
+                mSampleSelectedIndex = position;
+                mGLSurfaceView.setRenderMode(RENDERMODE_WHEN_DIRTY);
+
+                if (mRootView.getWidth() != mGLSurfaceView.getWidth()
+                        || mRootView.getHeight() != mGLSurfaceView.getHeight()) {
+                    mGLSurfaceView.setAspectRatio(mRootView.getWidth(), mRootView.getHeight());
+                }
+
+                mGLRender.setParamsInt(SAMPLE_TYPE, position + SAMPLE_TYPE, 0);
+
+                int sampleType = position + SAMPLE_TYPE;
+
+                switch (sampleType) {
+                    case SAMPLE_TYPE_TRIANGLE:
+                        break;
+
+                    default:
+                        break;
+                }
+
+                mGLSurfaceView.requestRender();
+
+                dialog.cancel();
+            }
+        });
+
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        resolutionsListView.setLayoutManager(manager);
+
+        resolutionsListView.setAdapter(myPreviewSizeViewAdapter);
+        resolutionsListView.scrollToPosition(mSampleSelectedIndex);
+
+        dialog.show();
+        dialog.getWindow().setContentView(rootView);
+
+    }
+
+
+    protected boolean hasPermissionsGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/byteflow/app/MyGLSurfaceView.java
@@ -0,0 +1,64 @@
+package com.byteflow.app;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+import android.util.Log;
+
+
+@SuppressLint("ViewConstructor")
+public class MyGLSurfaceView extends GLSurfaceView {
+    private static final String TAG = "MyGLSurfaceView";
+    private MyGLRender mGLRender;
+    private int mRatioWidth = 0;
+    private int mRatioHeight = 0;
+
+    public MyGLSurfaceView(Context context, MyGLRender glRender) {
+        this(context, glRender, null);
+    }
+
+    public MyGLSurfaceView(Context context, MyGLRender glRender, AttributeSet attrs) {
+        super(context, attrs);
+        this.setEGLContextClientVersion(2);
+        mGLRender = glRender;
+        setEGLConfigChooser(8, 8, 8, 8, 16, 8);
+        setRenderer(mGLRender);
+        setRenderMode(RENDERMODE_WHEN_DIRTY);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (0 == mRatioWidth || 0 == mRatioHeight) {
+            setMeasuredDimension(width, height);
+        } else {
+            if (width < height * mRatioWidth / mRatioHeight) {
+                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
+            } else {
+                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+            }
+        }
+    }
+
+    public void setAspectRatio(int width, int height) {
+        Log.d(TAG, "setAspectRatio() called with: width = [" + width + "], height = [" + height + "]");
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Size cannot be negative.");
+        }
+
+        mRatioWidth = width;
+        mRatioHeight = height;
+        requestLayout();
+    }
+
+    public MyGLRender getGLRender() {
+        return mGLRender;
+    }
+
+
+}
